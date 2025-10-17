01772d88f6a17d7eab53f9a27295ef639f912fc2
AnChunLun <yaboyerican@gmail.com>
2025-10-17 16:02:12 -0400

    Deleted NotNull import created by extracting method via intellij (MarkUs issue)

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -3,7 +3,6 @@ package dogapi;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
